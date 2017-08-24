eccac03cf7299b679f042eb595fe9622609db9cb
Kusakawa Lin <kusakawazeusu@gmail.com>
2017-08-24 16:12:48 +0800

    POPPER

--- resources/views/wireframe.blade.php
+++ resources/views/wireframe.blade.php
@@ -8,7 +8,7 @@
         {{--  Initialize Bootstrap  --}}
         <link rel="stylesheet" type="text/css" href="{{asset('css/bootstrap.min.css')}}">
         <script src="{{asset('js/jquery-3.2.1.slim.min.js')}}"></script>
-        <script src="{{asset('js/popper.js')}}"></script>
+        <script src="{{asset('js/popper.min.js')}}"></script>
         <script src="{{asset('js/bootstrap.min.js')}}"></script>
 
         {{--  Load my own css  --}}
