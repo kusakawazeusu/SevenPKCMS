a6d8566c763ee8a3282e39f4e746fdef7643d381
WWKING-GAMELAB\WWKING-GAMELAB <wu0851202@gmail.com>
2017-08-24 18:15:23 +0800

    Machine Monitor UI(假裝完成Ver)

--- app/Http/Controllers/MachineMonitorController.php
+++ app/Http/Controllers/MachineMonitorController.php
@@ -2,19 +2,15 @@
 
 namespace App\Http\Controllers;
 
-use View;
-use Illuminate\Support\Facades\Input;
 use Illuminate\Http\Request;
-use Redirect;
-use Validator;
+use App\Http\Controllers\Controller;
 
 class MachineMonitorController extends Controller
 {
-
+    //
     public function Index()
     {
-        $counters = 15;
-        return View::make('Machine.Monitor')
-            ->with('counters', $counters);
+        $i = 150;
+        return view('Machine.Monitor', ['counters'=>$i]);
     }
 }
--- resources/views/Machine/Monitor.blade.php
+++ resources/views/Machine/Monitor.blade.php
@@ -2,16 +2,26 @@
 
 @section('content')
 <script src ="{{asset('js/Machine/Monitor.js')}}"></script>
+<div class="jumbotron">
     <div class="row">
         @for ($i = 0; $i <= $counters; $i++)
-            <div class="col-lg-1">
-                <legend class="text-center">{{$i}}</legend>
-                <div class="text-center machine">
-                    <a id="a{{$i}}">
-                    <img id="{{$i}}"src="{{asset('img/machine/online.png')}}" width="100%" data-toggle="tooltip" data-placement="bottom" title="WTF">
-                    </a>
+            <div class="col-sm-1 mb-5">
+                <div id="{{$i}}" type="button" class="card machineCard" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" data-placement="right" title="使用者：餘額：">
+                    <img class="card-img-top" src="{{asset('img/machine/online.png')}}" alt="Card image cap">
+                    <div class="card-body">
+                        <h6 class="card-title">第{{$i}}台</h6>
+                        <p class="card-text">上線中</p>
+                    </div>
+                </div>                 
+                       
+                <div class="dropdown-menu" style="width:100%">
+                    <a class="dropdown-item" href="#">鍵入</a>
+                    <a class="dropdown-item" href="#">鍵出</a>
+                    <a class="dropdown-item" href="#">保留</a>
+                    <a class="dropdown-item" href="#">取消</a>
                 </div>
             </div>
         @endfor
     </div>
+</div>
 @endsection
