4304053efe9ba7573750908206b813631809f5c9
Kusakawa Lin <kusakawazeusu@gmail.com>
2017-08-25 17:53:51 +0800

    Operator UI 與 API Fetch

--- app/Http/Controllers/Operator.php
+++ app/Http/Controllers/Operator.php
@@ -2,12 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use Response;
 use Illuminate\Http\Request;
+use DB;
 
 class Operator extends Controller
 {
     function ShowOperator()
     {
         return view('Operator.operator');
+    }
+
+    function getOperators(Request $request)
+    {
+
+        $Page = $request->input('Page');
+        $ShowEntries = $request->input('ShowEntries');
+        $SearchText = $request->input('SearchText');
+
+        $operators = DB::table('operator')->select("Name","Account","Type","IDCardNumber","Phone")->where('Name','like','%'.$SearchText.'%')->limit($ShowEntries)->offset($Page*$ShowEntries)->get();
+
+        return Response::json($operators);
     }
 }
--- resources/views/Operator/operator.blade.php
+++ resources/views/Operator/operator.blade.php
@@ -5,14 +5,75 @@
 <script>
 
 $(document).ready(function() {
-    $('#OperatorTable').DataTable({
-        "paging":   false,
-        "info":     false,
-        "searching": false,
-        "bAutoWidth": false
+    
+    var t = $('#OperatorTable').DataTable({
+                "paging":   false,
+                "info":     false,
+                "searching": false,
+                "bAutoWidth": false,
+            });
+
+    var Page = 0;
+    var ShowEntries = 5;
+    var SeachText = "%";
+
+    // Initialize the table
+    GetData(ShowEntries,Page,SeachText);
+
+
+
+    /*
+        This function is used for getting data from API.
+
+        @params
+            - ShowEntries : Indicated the number of records we query from the API.
+            - Page : We needs this to calculate the data offset.
+            - SearchText : The keyword we're searching for.
+
+    */
+    function GetData(ShowEntries, Page, SearchText)
+    {
+        t.clear().draw();
+        var SendingData = { "ShowEntries":ShowEntries, "Page":Page, "SearchText":SearchText };
+
+        $.get("{{route('GetOperators')}}",SendingData,function(data){
+                
+                var NumOfData = data.length;
+
+                for( var i=0; i<NumOfData; i++ )
+                {
+                    var AccountTypeName;
+
+                    switch (data[i].Type)
+                    {
+                        case 0:
+                            AccountTypeName = "工作人員";
+                            break;
+                        case 1:
+                            AccountTypeName = "管理人員";
+                            break;
+                        case 2:
+                            AccountTypeName = "最高管理員";
+                            break;
+                    }
+
+                    t.row.add([
+                        data[i].Name,
+                        data[i].Account,
+                        AccountTypeName,
+                        data[i].IDCardNumber,
+                        data[i].Phone
+                    ]).draw(false);
+                }
+        });
+    }
+
+    $("#Name").keyup(function(){
+        SeachText = $(this).val();
+        GetData(ShowEntries,Page,SeachText);
     });
 
-} );
+});
 
 
 </script>
@@ -27,10 +88,25 @@ $(document).ready(function() {
         <button class="btn btn-primary">新增員工</button>
     </div>
 
-    <div class="col-md-3 mr-3">
-        <div class="input-group mb-2">
-            <div class="input-group-addon">姓名</div>
-            <input type="text" class="form-control" id="Name" placeholder="要搜尋的姓名 ...">
+    <div class="col-md-4 mr-3">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="input-group mb-2">
+                    <div class="input-group-addon">顯示筆數</div>
+                    <select class="form-control">
+                        <option>5</option>
+                        <option>10</option>
+                        <option>全部</option>
+                    </select>
+                </div>
+            </div>
+
+            <div class="col-md-6">
+                <div class="input-group mb-2">
+                    <div class="input-group-addon">姓名</div>
+                    <input type="text" class="form-control" id="Name" placeholder="要搜尋的姓名 ...">
+                </div>
+            </div>
         </div>
     </div>
 
