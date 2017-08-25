1be93d876183befa0f724d1cb15df97e9292afc7
Kusakawa Lin <kusakawazeusu@gmail.com>
2017-08-25 15:44:50 +0800

    安裝Datatable

--- resources/views/wireframe.blade.php
+++ resources/views/wireframe.blade.php
@@ -5,18 +5,18 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        {{--  Initialize Bootstrap  --}}
+        {{--  Initialize Some Framework  --}}
         <link rel="stylesheet" type="text/css" href="{{asset('css/bootstrap.min.css')}}">
         <link rel="stylesheet" type="text/css" href="{{asset('css/font-awesome.min.css')}}">
-
-
+        <link rel="stylesheet" type="text/css" href="{{asset('css/datatables.min.css')}}"/>
 
         <script src="{{asset('js/jquery-3.2.1.min.js')}}"></script>
         <script src="{{asset('js/popper.min.js')}}"></script>
         <script src="{{asset('js/bootstrap.min.js')}}"></script>
         <script src="{{asset('js/sweetalert2.min.js')}}"></script>
         <script src="{{asset('js/bootstrapValidator.js')}}"></script>
-           
+
+        <script src="{{asset('js/datatables.min.js')}}"></script>
 
         {{--  Load my own css  --}}
         <link rel="stylesheet" type="text/css" href="{{asset('css/wireframe.css')}}">
