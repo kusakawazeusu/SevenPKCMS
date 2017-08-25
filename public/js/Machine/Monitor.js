a5001ad49e7f563c63debff943a36e3900ab014b
WWKING-GAMELAB\WWKING-GAMELAB <wu0851202@gmail.com>
2017-08-25 14:55:58 +0800

    monitor

--- public/js/Machine/Monitor.js
+++ public/js/Machine/Monitor.js
@@ -3,6 +3,20 @@ $(function() {
     $('[data-toggle="dropdown"]').dropdown()
 })
 
-$(document).ready(function() {
+function CreditIn(id) {
+    console.log(id)
+}
 
+function CreditOn(id) {
+    console.log(id)
+}
+
+function GameReserved(id) {
+    console.log(id)
+}
+
+$(document).ready(function() {
+    $('.machineCard').on('show.bs.tooltip', function() {
+        $('.machineCard').attr('data-original-title', '使用者：<br>餘額：0')
+    })
 })
--- resources/views/Machine/Monitor.blade.php
+++ resources/views/Machine/Monitor.blade.php
@@ -5,20 +5,21 @@
 <div class="jumbotron">
     <div class="row">
         @for ($i = 0; $i <= $counters; $i++)
-            <div class="col-sm-1 mb-5">
-                <div id="{{$i}}" type="button" class="card machineCard" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" data-placement="right" title="使用者：餘額：">
-                    <img class="card-img-top" src="{{asset('img/machine/online.png')}}" alt="Card image cap">
+            <div class="col-sm-2 mb-5">
+                <div id="{{$i}}" type="button" class="card machineCard" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" data-placement="right" data-html="true"
+ title="使用者：<br/>餘額：">
+                    <img class="card-img-top" src="{{asset('img/machine/offline.png')}}" alt="Card image cap">
                     <div class="card-body">
                         <h6 class="card-title">第{{$i}}台</h6>
-                        <p class="card-text">上線中</p>
+                        <p class="card-text">離線中</p>
                     </div>
                 </div>                 
                        
-                <div class="dropdown-menu" style="width:100%">
-                    <a class="dropdown-item" href="#">鍵入</a>
-                    <a class="dropdown-item" href="#">鍵出</a>
-                    <a class="dropdown-item" href="#">保留</a>
-                    <a class="dropdown-item" href="#">取消</a>
+                <div class="dropdown-menu" style="width:100%" id="{{$i}}">
+                    <a class="dropdown-item" id="{{$i}}" onclick="CreditIn(this.id)">鍵入</a>
+                    <a class="dropdown-item" id="{{$i}}" onclick="CreditOn(this.id)">鍵出</a>
+                    <a class="dropdown-item" id="{{$i}}" onclick="GameReserved(this.id)">保留</a>
+                    <a class="dropdown-item">取消</a>
                 </div>
             </div>
         @endfor
