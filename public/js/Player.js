6696f6e54d77874686a972a750988d2ba57b864b
NTUST-FU\FU <asd5242539@gmail.com>
2017-08-28 16:41:09 +0800

    刪除會員完成

--- public/js/Player.js
+++ public/js/Player.js
@@ -1,37 +1,6 @@
-function DeletePlayer(ID)
-	{
-		console.log(ID);
-		swal({
-			title: 'Are you sure?',
-			text: "You won't be able to revert this!",
-			type: 'warning',
-			showCancelButton: true,
-			confirmButtonColor: '#3085d6',
-			cancelButtonColor: '#d33',
-			confirmButtonText: 'Yes, delete it!',
-			showLoaderOnConfirm: true,
-			preConfirm: function() {
-				return new Promise(function(resolve) {
-					$.ajax({
-						url: 'Player/DeletePlayer',
-						type: 'POST',
-						data: {ID: ID},
-					})
-					.done(function() {
-						swal(
-							'Deleted!',
-							'Your file has been deleted.',
-							'success'
-							)
-					})
-					.fail(function() {
-						console.log("error");
-					});					
-				});
-			},
-			allowOutsideClick: false     
-		});
-	}
+
+
+var playerTable;
 $(document).ready(function() {
 
 	$.ajaxSetup({
@@ -40,81 +9,33 @@ $(document).ready(function() {
 		}
 	});
 
+
 	////////////////////////////////////////
 	//玩家列表							  //
 	////////////////////////////////////////
 
-	var playerTable = $('#playerListTable').DataTable({
+	playerTable = $('#playerListTable').DataTable({
 		"paging":   false,
 		"info":     false,
 		"searching": false,
 		"bAutoWidth": false
 	});
 
-	var page=0;
-	var pagesNum = Math.ceil(entries / showNum);  // 記錄總共有幾頁
+	
 	$("#page").text('1');
 	$("#totalPage").text(pagesNum);
 
 
-	GetData(0);
+	GetData(page);
 
-	function GetData(page)
-	{
-		$.ajax({
-			url: 'Player/'+page+'/'+showNum,
-			type: 'GET',
-			data: {
-				name: $('.search#name').val(),
-				cardNumber:$('.search#cardNumber').val()
-			},
-		})
-		.done(function(response) {
-			console.log(response);
-			console.log("success");
-			playerTable.clear().draw();
-			for(i=0;i<response['players'].length;++i)
-			{
-				playerTable.row.add([
-					'<td style="text-align:center">'+
-					'<button class="btn btn-warning mr-1"><i class="fa fa-edit aria-hidden="true"></i></button>'+
-					"<button class='btn btn-danger mr-1' onclick='DeletePlayer("+response['players'][i].ID+")'><i class='fa fa-remove' aria-hidden='true'></i></button>"+
-					'</td>',
-					response['players'][i].CardNumber,
-					response['players'][i].Name,
-					response['players'][i].Balance,
-					response['players'][i].CardType,
-					response['players'][i].IDCardNumber,
-					response['players'][i].Cellphone,
-					response['players'][i].IntroducerID,
-					'<td style="text-align:center">'+
 
-					'<button class="btn btn-primary mr-1" id="edit"' + response['players'][i].ID+'"'+
-					'data-id="'+response['players'][i].ID+'" data-toggle="modal" data-target=""'+
-					'>證件</button>'+
+	
 
-					'<button class="btn btn-primary mr-1" id="edit"' + response['players'][i].ID+'"'+
-					'data-id="'+response['players'][i].ID+'" data-toggle="modal" data-target=""'+
-					'>證件反面</button>'+
-
-					'<button class="btn btn-primary mr-1" id="edit"' + response['players'][i].ID+'"'+
-					'data-id="'+response['players'][i].ID+'" data-toggle="modal" data-target=""'+
-					'>照片</button>'+
-					'</td>'
-					]).draw(false);
-
-			}
-			
-		})
-		.fail(function() {
-			console.log("error");
-		});		
-	}
-
-	$('#searchBtn').click(function(event) {
+	$('.deletePlayer').click(function(event) {
 		/* Act on the event */
-	});
 
+		DeletePlayer($(this).val());
+	});
 
 	$("#nextPage").click(function(){
 		page = page +1;
@@ -158,7 +79,6 @@ $(document).ready(function() {
 		}
 	});
 
-
 	$('#createPlayerSubmit').click(function(event) {
 		$.ajax({
 			url: 'Player/CreatePlayer',
@@ -177,7 +97,7 @@ $(document).ready(function() {
 		.done(function(response) {
 			console.log("success");
 			swal("新增員工成功","列表將自動更新。","success");
-			GetData(0);
+			GetData(page);
 			$('#createPlayer').modal('toggle');
 			$('.createInput').val('');
 			
@@ -185,8 +105,100 @@ $(document).ready(function() {
 		.fail(function() {
 			console.log("error");
 		});		
-	});
-
-
-	
+	});	
 });
+
+var page=0;
+var pagesNum = Math.ceil(entries / showNum);  // 記錄總共有幾頁
+
+function GetData(page)
+{
+	$.ajax({
+		url: 'Player/'+page+'/'+showNum,
+		type: 'GET',
+		data: {
+			name: $('.search#name').val(),
+			cardNumber:$('.search#cardNumber').val()
+		},
+	})
+	.done(function(response) {
+		playerTable.clear().draw();
+		for(i=0;i<response['players'].length;++i)
+		{
+			playerTable.row.add([
+				'<td style="text-align:center">'+
+				'<button class="btn btn-warning mr-1"><i class="fa fa-edit aria-hidden="true"></i></button>'+
+				'<button class="btn btn-danger deletePlayer mr-1" onclick=DeletePlayer("'+response['players'][i].ID+'")><i class="fa fa-remove" aria-hidden="true"></i></button>'+
+				'</td>',
+				response['players'][i].CardNumber,
+				response['players'][i].Name,
+				response['players'][i].Balance,
+				response['players'][i].CardType,
+				response['players'][i].IDCardNumber,
+				response['players'][i].Cellphone,
+				response['players'][i].IntroducerID,
+				'<td style="text-align:center">'+
+
+				'<button class="btn btn-primary mr-1" id="edit"' + response['players'][i].ID+'"'+
+				'data-id="'+response['players'][i].ID+'" data-toggle="modal" data-target=""'+
+				'>證件</button>'+
+
+				'<button class="btn btn-primary mr-1" id="edit"' + response['players'][i].ID+'"'+
+				'data-id="'+response['players'][i].ID+'" data-toggle="modal" data-target=""'+
+				'>證件反面</button>'+
+
+				'<button class="btn btn-primary mr-1" id="edit"' + response['players'][i].ID+'"'+
+				'data-id="'+response['players'][i].ID+'" data-toggle="modal" data-target=""'+
+				'>照片</button>'+
+				'</td>'
+				]).draw(false);
+		}
+
+		entries = response['numOfEntries'];
+			pagesNum = Math.ceil(entries / showNum);  // 記錄總共有幾頁
+
+			$("#totalPage").text(pagesNum);
+
+			$("#page").text(page+1);
+
+		})
+	.fail(function() {
+		console.log("error");
+	});
+}
+
+function DeletePlayer(ID)
+{
+	console.log(ID);
+	swal({
+		title: '確定刪除嗎?',
+		text: "將無法回復此人資料",
+		type: 'warning',
+		showCancelButton: true,
+		confirmButtonColor: '#3085d6',
+		cancelButtonColor: '#d33',
+		confirmButtonText: '是, 刪除!',
+		cancelButtonText:'取消',
+		showLoaderOnConfirm: true,
+		preConfirm: function() {
+			return new Promise(function(resolve) {
+				$.ajax({
+					url: 'Player/DeletePlayer',
+					type: 'POST',
+					data: {ID: ID},
+				})
+				.done(function() {
+					swal(
+						'Deleted!',
+						'Your file has been deleted.',
+						'success'
+						)
+					GetData(page);
+				})
+				.fail(function() {
+					console.log("error");
+				});					
+			});
+		}   
+	}).catch(swal.noop);
+}
--- resources/views/Player/player.blade.php
+++ resources/views/Player/player.blade.php
@@ -5,7 +5,7 @@
 <meta name="csrf-token" content="{{ csrf_token() }}" /><!-- 切記這兩行伊定要放在body最下面---->
 <script>
 	
-	var showNum = 10;
+	var showNum = 5;
 	
 	var entries = {{ $numOfEntries }};  // 紀錄總共有幾筆data
 </script>
