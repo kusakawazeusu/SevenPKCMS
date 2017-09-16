da0a93caae7e2025767c9689517fee68efaa5f82
WWKING-GAMELAB\WWKING-GAMELAB <wu0851202@gmail.com>
2017-09-16 17:28:26 +0800

    Machine Monitor updated(hope is final

--- public/js/Machine/Monitor.js
+++ public/js/Machine/Monitor.js
@@ -1,4 +1,4 @@
-$(function() {
+$(function () {
     $('[data-toggle="dropdown"]').dropdown();
     $('.machineCardTooltip').tooltip({
         html: true,
@@ -19,7 +19,7 @@ function CreditIn(id) {
         data: {
             id: id
         },
-        success: function(data) {
+        success: function (data) {
             maxDepositCredit = data.MaxDepositCredit;
             depositCreditOnce = data.DepositCreditOnce;
             playerPhone = data.Cellphone;
@@ -39,8 +39,8 @@ function CreditIn(id) {
         allowEscapeKey: false,
         showCancelButton: true,
         cancelButtonText: '取消',
-        preConfirm: function() {
-            return new Promise(function(resolve, reject) {
+        preConfirm: function () {
+            return new Promise(function (resolve, reject) {
                 $('#Credit').removeClass('swal2-inputerror');
                 $('#PlayerPhone').removeClass('swal2-inputerror');
                 if ($('#PlayerPhone').val() == '') {
@@ -51,15 +51,15 @@ function CreditIn(id) {
                     reject('請輸入金額');
                 } else {
                     $.ajax({
-                            url: 'Machine/Monitor/CheckCreditIn',
-                            type: 'POST',
-                            data: {
-                                PlayerPhone: $('#PlayerPhone').val(),
-                                Credit: $('#Credit').val(),
-                                machineID: id
-                            },
-                        })
-                        .done(function(response) {
+                        url: 'Machine/Monitor/CheckCreditIn',
+                        type: 'POST',
+                        data: {
+                            PlayerPhone: $('#PlayerPhone').val(),
+                            Credit: $('#Credit').val(),
+                            machineID: id
+                        },
+                    })
+                        .done(function (response) {
                             if (response.valid == 'true')
                                 resolve();
                             else if (response.errMsg == 'phone') {
@@ -76,39 +76,39 @@ function CreditIn(id) {
                                 reject('鍵入過多');
                             }
                         })
-                        .fail(function() {
+                        .fail(function () {
                             console.log("CheckCreditIn Error");
                         });
                 }
             });
         }
-    }).then(function() {
-            $.ajax({
-                url: 'Machine/Monitor/CreditIn',
-                type: 'post',
-                async: false,
-                data: {
-                    playerCellphone: $('#PlayerPhone').val(),
-                    credit: $('#Credit').val(),
-                    machineID: id,
-                    needCode: needCode,
-                    operatorID: operatorID //{{ Auth::user()->id }}
-                },
-                success: function(data) {
-                    if (data.done == 'success') {
-                        if (data.code != 0) swal("鍵入成功", "驗證碼：" + data.code, "success");
-                        else swal("鍵入成功", "", "success");
-                    } else {
-                        swal("鍵入失敗", data.errorMsg, "error");
-                    }
-                },
-                error: function(data) {
-                    console.log(data);
-                },
-            });
-            RefreshStatus();
-        },
-        function(dismiss) {
+    }).then(function () {
+        $.ajax({
+            url: 'Machine/Monitor/CreditIn',
+            type: 'post',
+            async: false,
+            data: {
+                playerCellphone: $('#PlayerPhone').val(),
+                credit: $('#Credit').val(),
+                machineID: id,
+                needCode: needCode,
+                operatorID: operatorID //{{ Auth::user()->id }}
+            },
+            success: function (data) {
+                if (data.done == 'success') {
+                    if (data.code != 0) swal("鍵入成功", "驗證碼：" + data.code, "success");
+                    else swal("鍵入成功", "", "success");
+                } else {
+                    swal("鍵入失敗", data.errorMsg, "error");
+                }
+            },
+            error: function (data) {
+                console.log(data);
+            },
+        });
+        RefreshStatus();
+    },
+        function (dismiss) {
             swal({
                 type: 'warning',
                 title: '取消鍵入'
@@ -118,13 +118,13 @@ function CreditIn(id) {
 
 function CreditOut(id) {
     $.ajax({
-            url: 'Machine/Monitor/GetCur',
-            type: 'POST',
-            data: {
-                id: id
-            },
-        })
-        .done(function(data) {
+        url: 'Machine/Monitor/GetCur',
+        type: 'POST',
+        data: {
+            id: id
+        },
+    })
+        .done(function (data) {
             if (data.Status != 0) {
                 swal({
                     title: '確定要鍵出嗎?',
@@ -135,7 +135,7 @@ function CreditOut(id) {
                     confirmButtonText: '是, 鍵出!',
                     cancelButtonText: '取消',
                     showLoaderOnConfirm: true
-                }).then(function() {
+                }).then(function () {
                     swal({
                         title: '請選擇交易方式',
                         text: '要取消交易直接點選空白處',
@@ -148,57 +148,57 @@ function CreditOut(id) {
                         showLoaderOnConfirm: true,
                         allowEscapeKey: false,
                         allowEnterKey: false
-                    }).then(function() {
-                            return new Promise(function(resolve) {
-                                $.ajax({
-                                        url: 'Machine/Monitor/CreditOut',
-                                        type: 'POST',
-                                        async: false,
-                                        data: {
-                                            ID: id,
-                                            type: 'ToCredit',
-                                            operatorID: operatorID //{{ Auth::user()->id }}
-                                        },
-                                    })
-                                    .done(function(data) {
-                                        if (data.done == 'success') {
-                                            swal('鍵出成功!', '會員餘額' + data.credit, 'success');
-                                        } else
-                                            swal('I have no idea', '', 'error');
-                                        RefreshStatus();
-                                    })
-                                    .fail(function() {
-                                        console.log("error");
-                                    });
-                            });
-                        },
-                        function(dismiss) {
+                    }).then(function () {
+                        return new Promise(function (resolve) {
+                            $.ajax({
+                                url: 'Machine/Monitor/CreditOut',
+                                type: 'POST',
+                                async: false,
+                                data: {
+                                    ID: id,
+                                    type: 'ToCredit',
+                                    operatorID: operatorID //{{ Auth::user()->id }}
+                                },
+                            })
+                                .done(function (data) {
+                                    if (data.done == 'success') {
+                                        swal('鍵出成功!', '會員餘額' + data.credit, 'success');
+                                    } else
+                                        swal('I have no idea', '', 'error');
+                                    RefreshStatus();
+                                })
+                                .fail(function () {
+                                    console.log("error");
+                                });
+                        });
+                    },
+                        function (dismiss) {
                             if (dismiss === 'cancel') {
                                 $.ajax({
-                                        url: 'Machine/Monitor/CreditOut',
-                                        type: 'POST',
-                                        async: false,
-                                        data: {
-                                            ID: id,
-                                            type: 'ToCash',
-                                            operatorID: operatorID //{{ Auth::user()->id }}
-                                        },
-                                    })
-                                    .done(function(data) {
+                                    url: 'Machine/Monitor/CreditOut',
+                                    type: 'POST',
+                                    async: false,
+                                    data: {
+                                        ID: id,
+                                        type: 'ToCash',
+                                        operatorID: operatorID //{{ Auth::user()->id }}
+                                    },
+                                })
+                                    .done(function (data) {
                                         if (data.done == 'success') {
                                             swal('兌現', '應付金額' + data.credit, 'info');
                                         } else
                                             swal('I have no idea', '', 'error');
                                         RefreshStatus();
                                     })
-                                    .fail(function() {
+                                    .fail(function () {
                                         console.log("error");
                                     });
                             } else {
                                 CreditOut(id);
                             }
                         })
-                }, function(dismiss) {
+                }, function (dismiss) {
                     swal('取消!', '', 'error');
                 });
             } else {
@@ -214,7 +214,7 @@ function RemoveReserved(id) {
         data: {
             id: id
         },
-    }).done(function(data) {
+    }).done(function (data) {
         if (data.Status == 2) {
             swal({
                 title: '確定要解除保留嗎?',
@@ -225,7 +225,7 @@ function RemoveReserved(id) {
                 confirmButtonText: '是, 解除保留!',
                 cancelButtonText: '取消',
                 showLoaderOnConfirm: true
-            }).then(function() {
+            }).then(function () {
                 swal({
                     title: '請選擇交易方式',
                     text: '要取消交易直接點選空白處',
@@ -238,57 +238,57 @@ function RemoveReserved(id) {
                     showLoaderOnConfirm: true,
                     allowEscapeKey: false,
                     allowEnterKey: false
-                }).then(function() {
-                        return new Promise(function(resolve) {
-                            $.ajax({
-                                    url: 'Machine/Monitor/RemoveReserved',
-                                    type: 'POST',
-                                    async: false,
-                                    data: {
-                                        ID: id,
-                                        type: 'FreeMachine',
-                                        operatorID: operatorID //{{ Auth::user()->id }}
-                                    },
-                                })
-                                .done(function(data) {
-                                    if (data.done == 'success') {
-                                        swal('鍵出成功!', '會員餘額' + data.credit, 'success');
-                                    } else
-                                        swal('I have no idea', '', 'error');
-                                    RefreshStatus();
-                                })
-                                .fail(function() {
-                                    console.log("error");
-                                });
-                        });
-                    },
-                    function(dismiss) {
+                }).then(function () {
+                    return new Promise(function (resolve) {
+                        $.ajax({
+                            url: 'Machine/Monitor/RemoveReserved',
+                            type: 'POST',
+                            async: false,
+                            data: {
+                                ID: id,
+                                type: 'FreeMachine',
+                                operatorID: operatorID //{{ Auth::user()->id }}
+                            },
+                        })
+                            .done(function (data) {
+                                if (data.done == 'success') {
+                                    swal('清除成功!', '會員餘額' + data.credit, 'success');
+                                } else
+                                    swal('I have no idea', '', 'error');
+                                RefreshStatus();
+                            })
+                            .fail(function () {
+                                console.log("error");
+                            });
+                    });
+                },
+                    function (dismiss) {
                         if (dismiss === 'cancel') {
                             $.ajax({
-                                    url: 'Machine/Monitor/RemoveReserved',
-                                    type: 'POST',
-                                    async: false,
-                                    data: {
-                                        ID: id,
-                                        type: 'RemoveReserved',
-                                        operatorID: operatorID //{{ Auth::user()->id }}
-                                    },
-                                })
-                                .done(function(data) {
+                                url: 'Machine/Monitor/RemoveReserved',
+                                type: 'POST',
+                                async: false,
+                                data: {
+                                    ID: id,
+                                    type: 'RemoveReserved',
+                                    operatorID: operatorID //{{ Auth::user()->id }}
+                                },
+                            })
+                                .done(function (data) {
                                     if (data.done == 'success') {
                                         swal('解除保留成功', '', 'success');
                                     } else
                                         swal('I have no idea', '', 'error');
                                     RefreshStatus();
                                 })
-                                .fail(function() {
+                                .fail(function () {
                                     console.log("error");
                                 });
                         } else {
                             CreditOut(id);
                         }
                     })
-            }, function(dismiss) {
+            }, function (dismiss) {
                 swal('取消!', '', 'error');
             });
         } else {
@@ -297,7 +297,7 @@ function RemoveReserved(id) {
     });
 }
 
-$(document).ready(function() {
+$(document).ready(function () {
 
     $.ajaxSetup({
         headers: {
@@ -307,7 +307,7 @@ $(document).ready(function() {
 
     RefreshStatus();
 
-    $('.machineCard').on('show.bs.tooltip', function() {
+    $('.machineCard').on('show.bs.tooltip', function () {
         $.ajax({
             url: 'Machine/Monitor/GetCur',
             type: 'post',
@@ -315,14 +315,14 @@ $(document).ready(function() {
             data: {
                 id: this.id
             },
-            success: function(data) {
+            success: function (data) {
                 if (data.Name != null) {
                     $('.machineCardTooltip').attr('data-original-title', '使用者：' + data.Name.toString() + '<br>餘額：' + data.CurCredit.toString());
                 } else {
                     $('.machineCardTooltip').attr('data-original-title', '使用者：無<br>餘額：0');
                 }
             },
-            error: function(data) {
+            error: function (data) {
                 console.log(data);
             },
         });
@@ -333,7 +333,7 @@ function RefreshStatus() {
     $.ajax({
         url: "Machine/Monitor/RefreshMachineStatus",
         type: 'POST',
-        success: function(data) {
+        success: function (data) {
             for (var i = 0; i < data.length; i++) {
                 if (data[i].Status == 2) {
                     $('#machine' + data[i].ID).attr("src", src = "img/machine/event.png");
@@ -347,10 +347,10 @@ function RefreshStatus() {
                 }
             }
         },
-        error: function(data) {}
+        error: function (data) { }
     });
 
-    setTimeout(function() {
+    setTimeout(function () {
         RefreshStatus();
         console.log("SetTimeOut");
     }, 180000);
