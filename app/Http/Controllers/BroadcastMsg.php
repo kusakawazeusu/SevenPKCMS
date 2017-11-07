af02a8eaa227482021d6343283bd611acccc5281
Kusakawa Lin <kusakawazeusu@gmail.com>
2017-11-07 18:00:15 +0800

    msg finished

--- app/Http/Controllers/BroadcastMsg.php
+++ app/Http/Controllers/BroadcastMsg.php
@@ -10,14 +10,34 @@ class BroadcastMsg extends Controller
 {
     public function MsgSetting()
     {
-        return view('BroadcastMsg');
+        $msg = DB::table('broadcastmsg')->where('id','1')->first();
+
+        return view('BroadcastMsg',['msg'=>$msg]);
     }
 
-    public function SetMsg()
+    public function SetMsg(Request $request)
     {
-        
+        DB::table('broadcastmsg')->where('id','1')->update([
+            'msg1' => $request->input('Msg1'),
+            'msg2' => $request->input('Msg2'),
+            'msg3' => $request->input('Msg3'),
+            'msg4' => $request->input('Msg4'),
+            'msg5' => $request->input('Msg5'),
+            'starttime1' => $request->input('Msg1StartTime'),
+            'starttime2' => $request->input('Msg2StartTime'),
+            'starttime3' => $request->input('Msg3StartTime'),
+            'starttime4' => $request->input('Msg4StartTime'),
+            'starttime5' => $request->input('Msg5StartTime'),
+            'endtime1' => $request->input('Msg1EndTime'),
+            'endtime2' => $request->input('Msg2EndTime'),
+            'endtime3' => $request->input('Msg3EndTime'),
+            'endtime4' => $request->input('Msg4EndTime'),
+            'endtime5' => $request->input('Msg5EndTime'),
+        ]);
 
-        return view('BroadcastMsg');
+        $msg = DB::table('broadcastmsg')->where('id','1')->first();
+
+        return view('BroadcastMsg',['msg'=>$msg]);
     }
     
 }
--- resources/views/BroadcastMsg.blade.php
+++ resources/views/BroadcastMsg.blade.php
@@ -14,6 +14,64 @@
         }
     }
 
+    $(document).ready(function() {
+
+        $("#SubBtn").click(function(event){
+            event.preventDefault();
+            
+            if( $("input[name='Msg1StartTime']").val() > $("input[name='Msg1EndTime']").val() )
+            {
+                swal({
+                    type: 'error',
+                    title: '設定錯誤',
+                    text: '訊息一時間設定錯誤！'
+                });
+            }
+            else if( $("input[name='Msg5StartTime']").val() > $("input[name='Msg5EndTime']").val() )
+            {
+                swal({
+                    type: 'error',
+                    title: '設定錯誤',
+                    text: '訊息五時間設定錯誤！'
+                });
+            }
+            else if( $("input[name='Msg4StartTime']").val() > $("input[name='Msg4EndTime']").val() )
+            {
+                swal({
+                    type: 'error',
+                    title: '設定錯誤',
+                    text: '訊息四時間設定錯誤！'
+                });
+            }
+            else if( $("input[name='Msg3StartTime']").val() > $("input[name='Msg3EndTime']").val() )
+            {
+                swal({
+                    type: 'error',
+                    title: '設定錯誤',
+                    text: '訊息三時間設定錯誤！'
+                });
+            }
+            else if( $("input[name='Msg2StartTime']").val() > $("input[name='Msg2EndTime']").val() )
+            {
+                swal({
+                    type: 'error',
+                    title: '設定錯誤',
+                    text: '訊息二時間設定錯誤！'
+                });
+            }
+            else
+            {
+                swal({
+                    type: 'success',
+                    title: '設定成功',
+                    showConfirmButton: false
+                });
+                $("#MsgForm").submit();
+            }
+        });
+
+    });
+
 </script>
 
 
@@ -21,85 +79,85 @@
 <hr>
 <br>
 
-<form method="POST" action="{{ route('SetMsg') }}">
+<form id="MsgForm" method="POST" action="{{ route('SetMsg') }}">
     {{ csrf_field() }}
 
     <div class="row">
         <div class="col-md-8">
             <label class="FormLabel">訊息一</label>
-            <input onkeyup="checkLength(this)" type="text" name="Msg1" class="form-control" required>
+            <input value="{{ $msg->msg1 }}" onkeyup="checkLength(this)" type="text" name="Msg1" class="form-control" required>
         </div>
         <div class="col-md-2">
                 <label class="FormLabel">開始時間</label>
-                <input type="time" name="Msg1StartTime" class="form-control" required>
+                <input value="{{ $msg->starttime1 }}" type="time" name="Msg1StartTime" class="form-control" required>
         </div>
         <div class="col-md-2">
                 <label class="FormLabel">結束時間</label>
-                <input type="time" name="Msg1EndTime" class="form-control" required>
+                <input value="{{ $msg->endtime1 }}" type="time" name="Msg1EndTime" class="form-control" required>
         </div>
     </div>
     <br>
     <div class="row">
         <div class="col-md-8">
             <label class="FormLabel">訊息二</label>
-            <input onkeyup="checkLength(this)" type="text" name="Msg2" class="form-control" required>
+            <input value="{{ $msg->msg2 }}" onkeyup="checkLength(this)" type="text" name="Msg2" class="form-control" required>
         </div>
         <div class="col-md-2">
             <label class="FormLabel">開始時間</label>
-            <input type="time" name="Msg2StartTime" class="form-control" required>
+            <input value="{{ $msg->starttime2 }}" type="time" name="Msg2StartTime" class="form-control" required>
         </div>
         <div class="col-md-2">
             <label class="FormLabel">結束時間</label>
-            <input type="time" name="Msg2EndTime" class="form-control" required>
+            <input value="{{ $msg->endtime2 }}" type="time" name="Msg2EndTime" class="form-control" required>
         </div>
     </div>
     <br>
     <div class="row">
         <div class="col-md-8">
             <label class="FormLabel">訊息三</label>
-            <input onkeyup="checkLength(this)" type="text" name="Msg3" class="form-control" required>
+            <input value="{{ $msg->msg3 }}" onkeyup="checkLength(this)" type="text" name="Msg3" class="form-control" required>
         </div>
         <div class="col-md-2">
             <label class="FormLabel">開始時間</label>
-            <input type="time" name="Msg3StartTime" class="form-control" required>
+            <input value="{{ $msg->starttime3 }}" type="time" name="Msg3StartTime" class="form-control" required>
         </div>
         <div class="col-md-2">
             <label class="FormLabel">結束時間</label>
-            <input type="time" name="Msg3EndTime" class="form-control" required>
+            <input value="{{ $msg->endtime3 }}" type="time" name="Msg3EndTime" class="form-control" required>
         </div>
     </div>
     <br>
     <div class="row">
         <div class="col-md-8">
             <label class="FormLabel">訊息四</label>
-            <input onkeyup="checkLength(this)" type="text" name="Msg4" class="form-control" required>
+            <input value="{{ $msg->msg4 }}" onkeyup="checkLength(this)" type="text" name="Msg4" class="form-control" required>
         </div>
         <div class="col-md-2">
             <label class="FormLabel">開始時間</label>
-            <input type="time" name="Msg4StartTime" class="form-control" required>
+            <input value="{{ $msg->starttime4 }}" type="time" name="Msg4StartTime" class="form-control" required>
         </div>
         <div class="col-md-2">
             <label class="FormLabel">結束時間</label>
-            <input type="time" name="Msg4EndTime" class="form-control" required>
+            <input value="{{ $msg->endtime4 }}" type="time" name="Msg4EndTime" class="form-control" required>
         </div>
     </div>
     <br>
     <div class="row">
         <div class="col-md-8">
             <label class="FormLabel">訊息五</label>
-            <input onkeyup="checkLength(this)" type="text" name="Msg5" class="form-control" required>
+            <input value="{{ $msg->msg5 }}" onkeyup="checkLength(this)" type="text" name="Msg5" class="form-control" required>
         </div>
         <div class="col-md-2">
             <label class="FormLabel">開始時間</label>
-            <input type="time" name="Msg5StartTime" class="form-control" required>
+            <input value="{{ $msg->starttime5 }}" type="time" name="Msg5StartTime" class="form-control" required>
         </div>
         <div class="col-md-2">
             <label class="FormLabel">結束時間</label>
-            <input type="time" name="Msg5EndTime" class="form-control" required>
+            <input value="{{ $msg->endtime5 }}" type="time" name="Msg5EndTime" class="form-control" required>
         </div>
     </div>
     <br>
-    <input type="submit" class="btn btn-primary btn-lg"></input>
+    <input type="submit" id="SubBtn" class="btn btn-primary btn-lg"></input>
 </form>
 
 
